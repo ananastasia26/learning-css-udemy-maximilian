9530a449bb5f823f9f0057a14fb0e4a7cb7c4cfb
Anastasia Leonova <nastyaleo98@yandex.ru>
2021-08-04 00:32:15 +0700

    Add showing mobile menu

--- shared.js
+++ shared.js
@@ -1,12 +1,31 @@
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
-var selectPlanButtons = document.querySelectorAll('.plan button');
+// console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
-     })
+  selectPlanButtons[i].addEventListener("click", function() {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
 }
 
-console.dir(selectPlanButtons);
+backdrop.addEventListener("click", function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
