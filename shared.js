05e7e80af8ecf0e0f67b10b9732d4a606bc58a64
Anastasia Leonova <nastyaleo98@yandex.ru>
2021-08-05 09:47:54 +0700

    Add menu into other pages

--- shared.js
+++ shared.js
@@ -7,30 +7,34 @@ var mobileNav = document.querySelector('.mobile-nav');
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-  selectPlanButtons[i].addEventListener("click", function() {
-   //  modal.style.display = "block";
-   //  backdrop.style.display = "block";
-   modal.classList.add('open');
-   backdrop.classList.add('open');
-  });
+   selectPlanButtons[i].addEventListener("click", function () {
+      //  modal.style.display = "block";
+      //  backdrop.style.display = "block";
+      modal.classList.add('open');
+      backdrop.classList.add('open');
+   });
 }
 
-backdrop.addEventListener("click", function() {
+backdrop.addEventListener("click", function () {
    //  mobileNav.style.display = 'none';
-    mobileNav.classList.remove('open');
-    closeModal();
+   mobileNav.classList.remove('open');
+   closeModal();
 });
 
-modalNoButton.addEventListener("click", closeModal);
-
-function closeModal() {
-//   backdrop.style.display = "none";
-//   modal.style.display = "none";
-   modal.classList.remove('open');
-   backdrop.classList.remove('open');  
+if (modalNoButton) {
+   modalNoButton.addEventListener("click", closeModal);
 }
 
-toggleButton.addEventListener('click', function() {
+function closeModal() {
+   //   backdrop.style.display = "none";
+   //   modal.style.display = "none";
+   if (modal) {
+      modal.classList.remove('open');
+   }
+   backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function () {
    //  mobileNav.style.display = 'block';
    //  backdrop.style.display = 'block';
    mobileNav.classList.add('open')
